1f5531f9ec0395cfb0ec829cd46d47a4b052dac7
sw1227 <32032108+sw1227@users.noreply.github.com>
2020-03-14 21:13:22 +0900

    Add first regl example

--- src/views/Sample.js
+++ src/views/Sample.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
+import createRegl from "regl";
 
 
 const Sample = () => {
+  const regl = createRegl(); // No arguments: creat a full screen canvas
 
-  return (
-    <div>sample</div>
-  );
+  const drawTriangle = regl({
+    frag: `
+    precision mediump float;
+    uniform vec4 color;
+    void main() {
+      gl_FragColor = color;
+    }`,
+
+    vert: `
+    precision mediump float;
+    attribute vec2 position;
+    void main() {
+      gl_Position = vec4(position, 0, 1);
+    }`,
+
+    attributes: {
+      position: [[0, -1], [-1, 0.5], [1, 1]] // No need to flatten
+    },
+
+    uniforms: {
+      color: regl.prop("color")
+    },
+    // Number of vertices to draw in this command
+    count: 3
+  });
+
+  useEffect(() => {
+    regl.frame(({ time }) => {
+      regl.clear({
+        color: [0, 0, 0, 0],
+        depth: 1
+      });
+      drawTriangle({
+        color: [
+          Math.cos(time * 1.0),
+          Math.sin(time * 0.8),
+          Math.cos(time * 3.0),
+          1
+        ]
+      });
+    });
+  }, []);
+
+  return <></>;
 };
 
 
